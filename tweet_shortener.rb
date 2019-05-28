ac9bd59da94cd597d84cd0eb65a23565b5d41a5f
Devin Arnold <dbarnold321@gmail.com>
2019-05-28 00:42:13 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -45,7 +45,7 @@ def shortened_tweet_truncator (tweet)
   if tweet.length > 140
     word_substituter(tweet)
     if tweet.length > 140
-      trunc_tweet =  tweet[0..140]
+      trunc_tweet =  tweet[0..139]
   else tweet
 end
 end
