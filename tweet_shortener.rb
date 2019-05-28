61e3ff44d24a7cc411cea20879b63ce7a76bad0e
Devin Arnold <dbarnold321@gmail.com>
2019-05-28 00:07:01 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" =>"&"
+   }
+end
+
+def word_substituter (tstring)
+  n_tweet = []
+  tstring.split.each do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+      n_tweet<< word
+    else
+      n_tweet<< word
+    end
+  end
+n_tweet.join(" ")
+end
+
+def bulk_tweet_shortener (array)
+  array.each do |i|
+    puts word_substituter(i)
+  end
+  array
+end
